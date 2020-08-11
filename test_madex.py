14d09a1e8705d4f62f6121d83fb77f9252a45d2b
Stavros Pitoglou <s.pitoglou@csl.gr>
2020-08-11 19:48:17 +0300

    First tests

---
+++ test_madex.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from sp_diab.madex import mean_adjusted_exponent_error
+
+
+class TestMadex(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_one_pair(self):
+        self.assertAlmostEqual(
+            mean_adjusted_exponent_error([50], [40]),
+            81.71,
+            2
+        )
