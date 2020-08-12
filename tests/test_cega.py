69202a9a8d2e2e18f0b295f0154b0ca800e2d5f0
Stavros Pitoglou <s.pitoglou@csl.gr>
2020-08-12 10:13:20 +0300

    Add cega tests

---
+++ tests/test_cega.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from ..sp_diab.cega import clarke_error_grid
+
+
+class TestCega(TestCase):
+
+    def test_zones(self):
+        y_true = [50, 60, 100, 135, 150, 200, 250, 300]
+        y_pred1 = [25, 45, 150, 160, 200, 300, 350, 390]
+        y_pred2 = [75, 75, 50, 110, 100, 100, 150, 210]
+        self.assertEqual(
+            clarke_error_grid(y_true, y_pred1, "")[1],
+            [3, 5, 0, 0, 0]
+        )
+        self.assertEqual(
+            clarke_error_grid(y_true, y_pred2, "")[1],
+            [1, 4, 0, 3, 0]
+        )
